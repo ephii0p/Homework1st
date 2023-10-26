aef2cb7f10d9acbed628b921d3a5fafb21bf57d2
ephii0p <belunkind@gmail.com>
2023-10-26 09:06:22 +0400

    Commit

---
+++ src/main/java/Culculeted.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Culculeted {
+    public static void main(String[] args) {
+        System.out.println("Напишите какая у вас фигура?");
+        String[] strings = new String[4];
+        strings[0] = "";
+        strings[1] = "1.круг";
+        strings[2] = "2.треугольник";
+        strings[3] = "3.квадрат";
+        for(int i = 0; i<strings.length; i++){
+            System.out.println(strings[i]);
+        }
+        Scanner j = new Scanner(System.in);
+        int p = j.nextInt();
+        if(p == 3){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Введите длину стороны");
+            double x = s.nextDouble();
+            double result = x*x;
+            System.out.println("Площадь квадрата ="+ result);
+        } else if(p == 1){Scanner s = new Scanner(System.in);
+            System.out.println("Введите длину радиуса");
+            double x = s.nextDouble();
+            double pi = 3.14;
+            double result = (double) ((x*x)*pi);
+            System.out.println("Площадь круга ="+ result);
+        } else if(p == 2){
+            Scanner s = new Scanner(System.in);
+            Scanner h = new Scanner(System.in);
+            System.out.println("Введите длину высоты");
+            double x = s.nextDouble();
+            System.out.println("Введите длину основания");
+            double y = h.nextDouble();
+            double z = 0.5;
+            double result = (double) (z * x * y);
+            System.out.println("Площадь треугольника =" + result);
+        }
+
+
+
+    }
+}
